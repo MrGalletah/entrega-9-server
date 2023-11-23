6ecce040d8d979a922cd60e04ace00ebafa5c091
MrGalletah <mdcabrera@hotmail.es>
2023-11-23 19:41:55 -0300

    segundo commit

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000;
 const app = express();
 const categories = require("./emercado-api-main/cats/cat.json")
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+   });
+
 
 app.get("/", (req, res) => {
        res.send("<h1>Bienvenid@ al servidor</h1>");
