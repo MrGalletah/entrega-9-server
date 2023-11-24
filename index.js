292a2e83e8323a7fcc54042d940a5d92a2cfe9b6
[MelaniePorley] <melanie.porley@gmail.com>
2023-11-23 21:03:57 -0300

    login endpoint added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const port = 3000;
+const KEY = "logginSuccess";
 
 const app = express();
-const categories = require("./emercado-api-main/cats/cat.json")
+const categories = require("./emercado-api-main/cats/cat.json");
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -22,6 +25,15 @@ app.get("/", (req, res) => {
   });
 
 
+app.post("/login", (req, res) => {
+  const {user, password} = req.body;   
+  if(user == "userVerified" && password == "passwordVerified") {
+    const token = jwt.sign({user}, KEY);
+    res.status(200).json({token});
+  } else {
+    res.status(401).json({messagge: "Usuario y/o contraseña incorrecta"});
+  };
+});
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
